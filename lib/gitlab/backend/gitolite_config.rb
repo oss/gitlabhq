329b4907a8e952e61ff882450a11f4d042bda293
KATIE POSKAITIS <katiepru@nbcs.rutgers.edu>
2013-01-30 19:03:38 -0500

    undoing hack

--- lib/gitlab/backend/gitolite_config.rb
+++ lib/gitlab/backend/gitolite_config.rb
@@ -165,9 +165,6 @@ module Gitlab
       repo.add_permission("RW+", "", name_writers) unless name_writers.blank?
       repo.add_permission("RW+", "", name_masters) unless name_masters.blank?
       
-	  # Allow users to access repos without ssh key (bugfix)
-	  repo.add_permission("RW+", "", Gitlab.config.gitolite.admin_key)
-
       # Add sharedRepository config
       repo.set_git_config("core.sharedRepository", "0660")
 
